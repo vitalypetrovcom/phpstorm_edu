4a7972ea7f28a1f677a76f986652dfe62c0f098f
Vitaly <vitaly.petrov.com@gmail.com>
2023-09-05 10:05:39 +0300

    Fist commit

---
+++ 01/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class TestController {
+
+    public $test;
+
+
+    public function test () {
+
+    }
+
+    public function test2 ( $a = 1 ) {
+        echo '123';
+    }
+
+    public function test3 () {
+
+    }
+
+    /**
+     * @param $test
+     */
+    public function test4 ( $test ) {
+        
+    }
+
+
+
+}
---
+++ 01/widgets/TestWidgets.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace app\widgets;
+
+
+class TestWidgets {
+
+}
